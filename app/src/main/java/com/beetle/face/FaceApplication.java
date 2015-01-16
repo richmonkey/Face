eb5bd13b9e16479781aca5cbfd1760d5beb41e46
houxh <houxuehua49@gmail.com>
2015-01-16 13:23:09 +0800

    网络状态监听

--- app/src/main/java/com/beetle/face/FaceApplication.java
+++ app/src/main/java/com/beetle/face/FaceApplication.java
@@ -1,6 +1,12 @@
 package com.beetle.face;
 
 import android.app.Application;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.Log;
 
 import com.beetle.NativeWebRtcContextRegistry;
@@ -46,5 +52,47 @@ public class FaceApplication  extends Application {
             im.setUid(Token.getInstance().uid);
             im.start();
         }
+
+        registerBroadcastReceiver();
+    }
+
+    public void registerBroadcastReceiver() {
+        this.registerReceiver(networdStateReceiver, new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"));
+    }
+
+
+    public void unregisterBroadcastReceiver() {
+        this.unregisterReceiver(networdStateReceiver);
+    }
+
+
+    BroadcastReceiver networdStateReceiver = new BroadcastReceiver(){
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (FaceApplication.this.isNetworkConnected(context)) {
+                if (Token.getInstance().uid > 0) {
+                    Log.i(TAG, "network connected start im service");
+                    IMService.getInstance().stop();
+                    IMService.getInstance().start();
+                }
+            } else {
+                Log.i(TAG, "network disconnected stop im service");
+                IMService.getInstance().stop();
+            }
+        }
+    };
+
+
+    public boolean isNetworkConnected(Context context) {
+        if (context != null) {
+            ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo mNetworkInfo = mConnectivityManager.getActiveNetworkInfo();
+            if (mNetworkInfo != null) {
+                return mNetworkInfo.isAvailable();
+            }
+        }
+        return false;
     }
 }
