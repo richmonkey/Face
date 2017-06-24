e0cf1c650bea62f3441c35c2699e535170d28b68
houxh <houxuehua49@gmail.com>
2017-06-24 18:53:07 +0800

    删除jitsi代码

--- react/features/conference/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Conference } from './Conference';
-export { default as ParticipantView } from './ParticipantView';
-export { default as ConferenceCreator } from './ConferenceCreator';
